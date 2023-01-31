3d751aa07295ff9026a3dc948e5cd3ce760fea3d
Five Grant <5@fivegrant.com>
2023-01-30 18:44:49 -0600

    Start moving to a REST service

--- src/depr.jl
+++ src/depr.jl
@@ -1,11 +1,14 @@
-module TA2toTA3Pipeline
+module SimService 
 
 using JSON
+using CSV
+using DataFrames
 using Catlab
 using Catlab.CategoricalAlgebra
 using AlgebraicPetri
 using AlgebraicPetri.BilayerNetworks
 #using AlgebraicPetri.ModelingToolkitInterop 
+using CSV
 import Catlab.CategoricalAlgebra: migrate!
 using Catlab.WiringDiagrams
 using Catlab.Programs.RelationalPrograms
@@ -36,8 +39,14 @@ function convert(::Type{ODEProblem}, sim_run::SimRun)
     return ODEProblem(ode, [200.0, 10.0, 2.0], sim_run.timestep, [0.4, 0.3])
 end
 
+gen_ode_problem(sim_run::SimRun) = convert(ODEProblem, sim_run)
 
-function load_into_ode_prob(path::String)
+gen_ode_problem(path::String)::ODEProblem = gen_ode_problem(SimRun(path, (0,0), (), ()))
+
+# TODO: Consider not including this as a method of `solve`
+solve(path::String) = EasyModelAnalysis.solve(gen_ode_problem(path))
+
+function load_into_ode_prob(path::String, tspan::Tuple{Float64, Float64})
 
     # TODO: Use IOBuffer instead of writing a temp file
     file = open(path, "r")
@@ -45,7 +54,7 @@ function load_into_ode_prob(path::String)
     close(file)
     touch("temp.json")
     open("temp.json", "w") do file
-        write(file, JSON.json(request["petri"]))
+        write(file, JSON.json(request["petri"])) # ACSET READS REQUIRE FILE
     end
 
     # Get the actual ODE
@@ -67,16 +76,24 @@ function load_into_ode_prob(path::String)
     #u0 = Dict( for name in petri[:, :sname])
     #p = get_params.(petri[:, :tname])
 
-    return ODEProblem(ode, u0, (0,90), p)
+    return ODEProblem(ode, u0, tspan, p)
 end
 
+function get_dataset_for_fit(dataset_path::String, sim_path::String, date_col::Symbol, name_map::Vector{Tuple{Symbol, Symbol}})
+    df = CSV.read(dataset_path, DataFrame)
+    sort!(df, date_col)
+    entries = first(size(df))
+    df[!, :steps] = 0: entries - 1
 
-gen_ode_problem(sim_run::SimRun) = convert(ODEProblem, sim_run)
+    prob = load_into_ode_prob(sim_path, (0.0, Float64(entries - 1)))
 
-gen_ode_problem(path::String)::ODEProblem = gen_ode_problem(SimRun(path, (0,0), (), ()))
+    rename!(df, Dict(name_map))
 
-# TODO: Consider not including this as a method of `solve`
-solve(path::String) = EasyModelAnalysis.solve(gen_ode_problem(path))
+    #df[:, [:symbol1, symbol2]]
+    data = Array(df[:, map(last, name_map)])
+
+    return (data, prob)
+end
 
 
 end # module Pipeline
